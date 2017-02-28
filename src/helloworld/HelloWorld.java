00a3d027e59a736eb4a1b6dbd9adc6f15dc04437
juan <juan@192.168.0.17>
2017-02-28 11:28:56 -0800

    Bugarin714 CS112 bugarin juan HelloWorld app

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author juan bugarin cs112 wednesday 6:30 pm
+ */
+public class HelloWorld{ 
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Santa Ana College");
+		}
+}
